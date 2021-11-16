01b676e13355c43f6529ce93fa1855022b075a77
jxstxn1 <justin@phntm.xyz>
2021-11-16 14:18:18 +0100

    Fix script and make {{name}} executable

--- sidekick/tool/generateTemplate.sh
+++ sidekick/tool/generateTemplate.sh
@@ -1,5 +1,5 @@
 #!/usr/bin/env bash
 
 echo "Generating Sidekick Template Bundle"
-mason bundle cli_template/bricks/cli -t dart -o lib/src/templates/
+mason bundle ../cli_template/bricks/cli -t dart -o lib/src/templates/
 mv lib/src/templates/cli_bundle.dart lib/src/templates/cli_bundle.g.dart
