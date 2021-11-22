30df8bfd18084fc0bf0f0250ed46be2ecd098fbf
jxstxn1 <justin@phntm.xyz>
2021-11-22 17:41:23 +0100

    Rename Cli to sidekick

--- sidekick/cli_template/bricks/cli/__brick__/packages/{{#lowerCase}}{{name}}{{/lowerCase}}_cli/lib/src/{{#lowerCase}}{{name}}{{/lowerCase}}_command_runner.dart
+++
@@ -1,6 +0,0 @@
-import 'package:args/command_runner.dart';
-
-class {{#titleCase}}{{name}}{{/titleCase}}CommandRunner extends CommandRunner {
-  {{#titleCase}}{{name}}{{/titleCase}}CommandRunner()
-      : super('Sidekick', 'A convenience script that simplifies setting up and running your project project.');
-}
