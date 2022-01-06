b8d27bc1eb133b1734e4eab1d4416f5df93738ea
Pascal Welsch <pascal@phntm.xyz>
2022-01-06 14:53:22 +0100

    Format generated code

--- sidekick/tool/generateTemplate.sh
+++ sidekick/tool/generateTemplate.sh
@@ -4,3 +4,4 @@ set -x
 echo "Generating Sidekick Template Bundle"
 mason bundle cli_template/bricks/sidekick -t dart -o lib/src/templates/
 mv lib/src/templates/sidekick_bundle.dart lib/src/templates/cli_bundle.g.dart
+dart format sidekick/lib/src/templates/cli_bundle.g.dart
