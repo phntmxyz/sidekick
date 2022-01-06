23cfa8c2a711b4aaaf0a4978df662eec91c8b9fc
Pascal Welsch <pascal@phntm.xyz>
2022-01-06 14:55:48 +0100

    Fix path

--- sidekick/tool/generateTemplate.sh
+++ sidekick/tool/generateTemplate.sh
@@ -1,7 +1,7 @@
 #!/usr/bin/env bash
-set -x
+set -e
 
 echo "Generating Sidekick Template Bundle"
 mason bundle cli_template/bricks/sidekick -t dart -o lib/src/templates/
 mv lib/src/templates/sidekick_bundle.dart lib/src/templates/cli_bundle.g.dart
-dart format sidekick/lib/src/templates/cli_bundle.g.dart
+dart format lib/src/templates/cli_bundle.g.dart
