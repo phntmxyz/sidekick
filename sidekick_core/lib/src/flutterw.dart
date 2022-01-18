dc447988c3319989738a687a6e68bb619fb4445f
Pascal Welsch <pascal@phntm.xyz>
2022-01-18 15:30:33 +0100

    Explicitly call bash on windows

--- sidekick_core/lib/src/flutterw.dart
+++ sidekick_core/lib/src/flutterw.dart
@@ -12,13 +12,25 @@ int flutterw(
   print('Assuming flutterw located in ${repository.root.path}');
   final flutterw = repository.root.file('flutterw');
 
-  final process = dcli.startFromArgs(
-    flutterw.path,
-    args,
-    workingDirectory: workingDir.path,
-    nothrow: true,
-    progress: progress,
-    terminal: progress == null,
-  );
-  return process.exitCode ?? -1;
+  if (Platform.isWindows) {
+    final process = dcli.startFromArgs(
+      'bash',
+      [flutterw.path, ...args],
+      workingDirectory: workingDir.path,
+      nothrow: true,
+      progress: progress,
+      terminal: progress == null,
+    );
+    return process.exitCode ?? -1;
+  } else {
+    final process = dcli.startFromArgs(
+      flutterw.path,
+      args,
+      workingDirectory: workingDir.path,
+      nothrow: true,
+      progress: progress,
+      terminal: progress == null,
+    );
+    return process.exitCode ?? -1;
+  }
 }
