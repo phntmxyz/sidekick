c545add8903fee2abb908ec23bd4bf322a0724e0
Pascal Welsch <pascal@phntm.xyz>
2022-01-19 14:22:10 +0100

    Regenerate bundle

--- sidekick/lib/src/templates/cli_bundle.g.dart
+++ sidekick/lib/src/templates/cli_bundle.g.dart
@@ -1,5 +1,5 @@
 // GENERATED CODE - DO NOT MODIFY BY HAND
-// ignore_for_file: prefer_single_quotes, public_member_api_docs, lines_longer_than_80_chars, implicit_dynamic_list_literal
+// ignore_for_file: prefer_single_quotes, public_member_api_docs, lines_longer_than_80_chars, implicit_dynamic_list_literal, implicit_dynamic_map_literal
 
 import 'package:mason/mason.dart';
 
@@ -78,5 +78,8 @@ final sidekickBundle = MasonBundle.fromJson(<String, dynamic>{
   "hooks": [],
   "name": "sidekick",
   "description": "Creates the sidekick basic structure.",
-  "vars": ["name"]
+  "version": "1.0.0",
+  "vars": {
+    "name": {"type": "string"}
+  }
 });
