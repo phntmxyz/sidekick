fc62ecd0d85e5555f2ff7839c9f8b67d7b89e4b3
Pascal Welsch <pascal@phntm.xyz>
2022-01-24 21:34:28 +0100

    Fix private constructor generation

--- sidekick/cli_template/bricks/sidekick/__brick__/packages/{{#lowerCase}}{{name}}{{/lowerCase}}_sidekick/lib/src/{{#lowerCase}}{{name}}{{/lowerCase}}_project.dart
+++ sidekick/cli_template/bricks/sidekick/__brick__/packages/{{#lowerCase}}{{name}}{{/lowerCase}}_sidekick/lib/src/{{#lowerCase}}{{name}}{{/lowerCase}}_project.dart
@@ -6,7 +6,7 @@ class {{#titleCase}}{{name}}{{/titleCase}}Project extends DartPackage {
     return {{#titleCase}}{{name}}{{/titleCase}}Project._(package.root, package.name);
   }
 
-  {{#titleCase}}{{name}}{{/titleCase}}Project(Directory root) : super.flutter(root, name);
+  {{#titleCase}}{{name}}{{/titleCase}}Project._(Directory root, String name) : super.flutter(root, name);
 
   DartPackage get {{#lowerCase}}{{name}}{{/lowerCase}}SidekickPackage => DartPackage.fromDirectory(root.directory('packages/{{#lowerCase}}{{name}}{{/lowerCase}}_sidekick'))!;
 
