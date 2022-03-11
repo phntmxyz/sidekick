7860a20043b64424f79725ffecb4d0a8893658be
Pascal Welsch <pascal@phntm.xyz>
2022-03-11 20:11:11 +0100

    Make maskSecret public

--- sidekick_vault/lib/src/logging.dart
+++ sidekick_vault/lib/src/logging.dart
@@ -1,8 +1,8 @@
 import 'package:actions_toolkit_dart/core.dart' as gha;
 import 'package:sidekick_core/sidekick_core.dart';
 
-/// Hides the secret on CI, replaces it with ***
-void hideSecret(String secret) {
+/// Hides the secret on GitHub Actions, replaces it with '***'
+void maskSecret(String secret) {
   if (env['GITHUB_ACTIONS'] == 'true') {
     // hide secret on github actions
     gha.setSecret(secret: secret);
