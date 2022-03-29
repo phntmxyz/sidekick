711c961c7559d6ff9fe5e340d1d59a8383b55180
Pascal Welsch <pascal@phntm.xyz>
2022-03-29 14:37:52 +0200

    Add hint how to debug the cli

--- sidekick/test/util/cli_runner.dart
+++ sidekick/test/util/cli_runner.dart
@@ -1,12 +1,21 @@
 import 'dart:io';
 
+import 'package:sidekick_core/sidekick_core.dart';
 import 'package:test_process/test_process.dart';
+import 'package:sidekick/sidekick.dart' as sidekick;
 
-/// Executes the CLI in a separate process
+/// Executes the CLI in a separate process so it doesn't interfere with
+/// singletons in the test like the [entryWorkingDirectory]
 Future<TestProcess> sidekickCli(
   List<String> args, {
   required Directory workingDirectory,
-}) {
+}) async {
+  // Use this to debug the sidekick CLI
+  // await IOOverrides.runZoned(
+  //   () => sidekick.main(args),
+  //   getCurrentDirectory: () => workingDirectory,
+  // );
+
   return TestProcess.start(
     'dart',
     ['${Directory.current.path}/bin/sidekick.dart', ...args],
