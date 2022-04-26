3f9850d857aeb92cee36cbb72af288fb4caffc26
minhqdao <asiancorporator@yahoo.de>
2022-04-26 20:39:19 +0200

    Extract SidekickCommandRunner

--- sidekick/lib/sidekick.dart
+++ sidekick/lib/sidekick.dart
@@ -1,14 +1,7 @@
-import 'package:args/command_runner.dart';
 import 'package:sidekick/src/init/init_command.dart';
+import 'package:sidekick/src/init/sidekick_command_runner.dart';
 
 Future<void> main(List<String> args) async {
   final runner = SidekickCommandRunner()..addCommand(InitCommand());
   await runner.run(args);
-}
-
-class SidekickCommandRunner extends CommandRunner {
-  SidekickCommandRunner() : super('sidekick', _desc);
-
-  static const _desc =
-      'Generator for a sidekick command line application (cli)';
 }
