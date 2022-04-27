5b6777de5d7d89b042d4fa5f23f78f3a66a35105
minhqdao <asiancorporator@yahoo.de>
2022-04-27 10:41:15 +0200

    Revert and make private

--- sidekick/lib/sidekick.dart
+++ sidekick/lib/sidekick.dart
@@ -1,7 +1,14 @@
+import 'package:args/command_runner.dart';
 import 'package:sidekick/src/init/init_command.dart';
-import 'package:sidekick/src/init/sidekick_command_runner.dart';
 
 Future<void> main(List<String> args) async {
-  final runner = SidekickCommandRunner()..addCommand(InitCommand());
+  final runner = _SidekickCommandRunner()..addCommand(InitCommand());
   await runner.run(args);
+}
+
+class _SidekickCommandRunner extends CommandRunner {
+  _SidekickCommandRunner() : super('sidekick', _desc);
+
+  static const _desc =
+      'Generator for a sidekick command line application (cli)';
 }
