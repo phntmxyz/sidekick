d85653f7a07fa674fd9d444a3798a18be7c28efb
Minh Dao <43783196+minhqdao@users.noreply.github.com>
2022-04-27 10:59:56 +0200

    Extract command runner and update readme (#39)
    Co-authored-by: Pascal Welsch <pascal@phntm.xyz>

--- sidekick/lib/sidekick.dart
+++ sidekick/lib/sidekick.dart
@@ -2,12 +2,12 @@ import 'package:args/command_runner.dart';
 import 'package:sidekick/src/init/init_command.dart';
 
 Future<void> main(List<String> args) async {
-  final runner = SidekickCommandRunner()..addCommand(InitCommand());
+  final runner = _SidekickCommandRunner()..addCommand(InitCommand());
   await runner.run(args);
 }
 
-class SidekickCommandRunner extends CommandRunner {
-  SidekickCommandRunner() : super('sidekick', _desc);
+class _SidekickCommandRunner extends CommandRunner {
+  _SidekickCommandRunner() : super('sidekick', _desc);
 
   static const _desc =
       'Generator for a sidekick command line application (cli)';
