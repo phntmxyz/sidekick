b791567ca9797d86c564ace27226ef37703641ec
Pascal Welsch <pascal@phntm.xyz>
2022-04-27 10:42:23 +0200

    Make _SidekickCommandRunner private

--- sidekick/lib/src/init/sidekick_command_runner.dart
+++
@@ -1,8 +0,0 @@
-import 'package:args/command_runner.dart';
-
-class SidekickCommandRunner extends CommandRunner {
-  SidekickCommandRunner() : super('sidekick', _desc);
-
-  static const _desc =
-      'Generator for a sidekick command line application (cli)';
-}
