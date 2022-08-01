112fc82d696339a5bb93b2db99a95dea204458ef
Pascal Welsch <pascal@phntm.xyz>
2022-08-01 11:42:54 +0200

    Update actions_toolkit_dart

--- sidekick_vault/lib/src/logging.dart
+++ sidekick_vault/lib/src/logging.dart
@@ -1,10 +1,10 @@
-import 'package:actions_toolkit_dart/core.dart' as gha;
+import 'package:actions_toolkit_dart/core.dart' as core;
 import 'package:sidekick_core/sidekick_core.dart';
 
 /// Hides the secret on GitHub Actions, replaces it with '***'
 void maskSecret(String secret) {
   if (env['GITHUB_ACTIONS'] == 'true') {
     // hide secret on github actions
-    gha.setSecret(secret);
+    core.setSecret(secret);
   }
 }
