ce7dc846a424d3e70afce6d4e6b62f7d09e4cf75
Pascal Welsch <pascal@phntm.xyz>
2022-09-16 16:18:42 +0200

    Update to support arm64 (apple silicone)

--- sidekick/tool/download_dart.sh
+++ sidekick/tool/download_dart.sh
@@ -1,6 +1,8 @@
+#!/usr/bin/env bash
+
 # Downloads the dart sdk
 
-# Highly inspired by https://github.com/flutter/flutter/blob/bde9f11831f60ef35ab15d69bc37fb47e04b0ee1/bin/internal/update_dart_sdk.sh
+# Highly inspired by https://github.com/flutter/flutter/blob/b7b8b759bc3ab7a80d2576d52f7b05bc1e6e23bd/bin/internal/update_dart_sdk.sh
 
 
 set -e
@@ -9,14 +11,15 @@ SIDEKICK_PACKAGE_ROOT="$(dirname "$(dirname "$(dirname "${BASH_SOURCE[0]}")")")"
 
 DART_SDK_PATH="$SIDEKICK_PACKAGE_ROOT/build/cache/dart-sdk"
 DART_SDK_PATH_OLD="$DART_SDK_PATH.old"
-DART_VERSION="2.14.4"
+DART_VERSION="2.18.1"
 DART_VERSION_STAMP="$SIDEKICK_PACKAGE_ROOT/build/cache/dartsdk.stamp"
+OS="$(uname -s)"
 
 if [ ! -f "$DART_VERSION_STAMP" ] || [ "$DART_VERSION" != `cat "$DART_VERSION_STAMP"` ]; then
   command -v curl > /dev/null 2>&1 || {
     >&2 echo
     >&2 echo 'Missing "curl" tool. Unable to download Dart SDK.'
-    case "$(uname -s)" in
+    case "$OS" in
       Darwin)
         >&2 echo 'Consider running "brew install curl".'
         ;;
@@ -33,7 +36,7 @@ if [ ! -f "$DART_VERSION_STAMP" ] || [ "$DART_VERSION" != `cat "$DART_VERSION_ST
   command -v unzip > /dev/null 2>&1 || {
     >&2 echo
     >&2 echo 'Missing "unzip" tool. Unable to extract Dart SDK.'
-    case "$(uname -s)" in
+    case "$OS" in
       Darwin)
         echo 'Consider running "brew install unzip".'
         ;;
@@ -47,20 +50,42 @@ if [ ! -f "$DART_VERSION_STAMP" ] || [ "$DART_VERSION" != `cat "$DART_VERSION_ST
     echo
     exit 1
   }
-  >&2 echo "Downloading Dart SDK from Flutter engine $ENGINE_VERSION..."
 
-  # On x64 stdout is "uname -m: x86_64"
-  # On arm64 stdout is "uname -m: aarch64, arm64_v8a"
-  case "$(uname -m)" in
-    x86_64)
-      ARCH="x64"
-      ;;
-    *)
-      ARCH="arm64"
-      ;;
-  esac
+  # `uname -m` may be running in Rosetta mode, instead query sysctl
+  if [ "$OS" = 'Darwin' ]; then
+    # Allow non-zero exit so we can do control flow
+    set +e
+    # -n means only print value, not key
+    QUERY="sysctl -n hw.optional.arm64"
+    # Do not wrap $QUERY in double quotes, otherwise the args will be treated as
+    # part of the command
+    QUERY_RESULT=$($QUERY 2>/dev/null)
+    if [ $? -eq 1 ]; then
+      # If this command fails, we're certainly not on ARM
+      ARCH='x64'
+    elif [ "$QUERY_RESULT" = '0' ]; then
+      # If this returns 0, we are also not on ARM
+      ARCH='x64'
+    elif [ "$QUERY_RESULT" = '1' ]; then
+      ARCH='arm64'
+    else
+      >&2 echo "'$QUERY' returned unexpected output: '$QUERY_RESULT'"
+      exit 1
+    fi
+    set -e
+  else
+    # On x64 stdout is "uname -m: x86_64"
+    # On arm64 stdout is "uname -m: aarch64, arm64_v8a"
+    case "$(uname -m)" in
+      x86_64)
+        ARCH="x64"
+        ;;
+      *)
+        ARCH="arm64"
+        ;;
+    esac
+  fi
 
-  case "$(uname -s)" in
     Darwin)
       DART_ZIP_NAME="dartsdk-macos-${ARCH}-release.zip"
       IS_USER_EXECUTABLE="-perm +100"
@@ -79,6 +104,8 @@ if [ ! -f "$DART_VERSION_STAMP" ] || [ "$DART_VERSION" != `cat "$DART_VERSION_ST
       ;;
   esac
 
+  >&2 echo "Downloading $OS $ARCH Dart SDK $DART_VERSION..."
+
   # Use the default find if possible.
   if [ -e /usr/bin/find ]; then
     FIND=/usr/bin/find
@@ -86,7 +113,8 @@ if [ ! -f "$DART_VERSION_STAMP" ] || [ "$DART_VERSION" != `cat "$DART_VERSION_ST
     FIND=find
   fi
 
- DART_SDK_URL="https://storage.googleapis.com/dart-archive/channels/stable/release/$DART_VERSION/sdk/$DART_ZIP_NAME"
+  DART_SDK_BASE_URL="${GOOGLE_STORAGE_BASE_URL:-https://storage.googleapis.com}"
+  DART_SDK_URL="$DART_SDK_BASE_URL/dart-archive/channels/stable/release/$DART_VERSION/sdk/$DART_ZIP_NAME"
 
   # if the sdk path exists, copy it to a temporary location
   if [ -d "$DART_SDK_PATH" ]; then
@@ -97,22 +125,35 @@ if [ ! -f "$DART_VERSION_STAMP" ] || [ "$DART_VERSION" != `cat "$DART_VERSION_ST
   # install the new sdk
   rm -rf -- "$DART_SDK_PATH"
   mkdir -m 755 -p -- "$DART_SDK_PATH"
-  DART_SDK_ZIP="$SIDEKICK_PACKAGE_ROOT/build/cache/$DART_ZIP_NAME"
+  DART_SDK_ZIP_FOLDER="$SIDEKICK_PACKAGE_ROOT/build/cache"
+  DART_SDK_ZIP="$DART_SDK_ZIP_FOLDER/$DART_ZIP_NAME"
 
-  # Conditionally set verbose flag for LUCI
-  verbose_curl=""
-  if [[ -n "$LUCI_CI" ]]; then
-    verbose_curl="--verbose"
-  fi
+  # Create cache folder when it doesn't exits
+  mkdir -p "$DART_SDK_ZIP_FOLDER"
 
-  curl ${verbose_curl} --retry 3 --continue-at - --location --output "$DART_SDK_ZIP" "$DART_SDK_URL" 2>&1 || {
+  curl --retry 3 --continue-at - --location --output "$DART_SDK_ZIP" "$DART_SDK_URL" 2>&1 || {
+    curlExitCode=$?
+    # Handle range errors specially: retry again with disabled ranges (`--continue-at -` argument)
+    # When this could happen:
+    # - missing support of ranges in proxy servers
+    # - curl with broken handling of completed downloads
+    #   This is not a proper fix, but doesn't require any user input
+    # - mirror of flutter storage without support of ranges
+    #
+    # 33  HTTP range error. The range "command" didn't work.
+    # https://man7.org/linux/man-pages/man1/curl.1.html#EXIT_CODES
+    if [ $curlExitCode != 33 ]; then
+      return $curlExitCode
+    fi
+    curl --retry 3 --location --output "$DART_SDK_ZIP" "$DART_SDK_URL" 2>&1
+  } || {
     >&2 echo
     >&2 echo "Failed to retrieve the Dart SDK from: $DART_SDK_URL"
     >&2 echo
     rm -f -- "$DART_SDK_ZIP"
     exit 1
   }
-  unzip -o -q "$DART_SDK_ZIP" -d "$SIDEKICK_PACKAGE_ROOT/build/cache/" || {
+  unzip -o -q "$DART_SDK_ZIP" -d "$SIDEKICK_PACKAGE_ROOT/build/cache" || {
     >&2 echo
     >&2 echo "It appears that the downloaded file is corrupt; please try again."
     >&2 echo
@@ -121,7 +162,7 @@ if [ ! -f "$DART_VERSION_STAMP" ] || [ "$DART_VERSION" != `cat "$DART_VERSION_ST
   }
   rm -f -- "$DART_SDK_ZIP"
   $FIND "$DART_SDK_PATH" -type d -exec chmod 755 {} \;
-  $FIND "$DART_SDK_PATH" -type f ${IS_USER_EXECUTABLE} -exec chmod a+x,a+r {} \;
+  $FIND "$DART_SDK_PATH" -type f $IS_USER_EXECUTABLE -exec chmod a+x,a+r {} \;
   echo "$DART_VERSION" > "$DART_VERSION_STAMP"
 
   # delete any temporary sdk path
