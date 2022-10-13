1cd5debd3bb34b0a3c7dfa11d2a86403e6f9be07
Giuseppe Cianci <giuseppe.cianci97@gmail.com>
2022-10-13 16:44:21 +0200

    refactor

--- sidekick/cli_template/bricks/package/__brick__/lib/src/commands/plugins_command.dart
+++ sidekick/cli_template/bricks/package/__brick__/lib/src/commands/plugins_command.dart
@@ -88,132 +88,32 @@ class AddPluginsCommand extends Command {
     }
 
     /// depends on [source]
-    /// 'hosted' -> package name; 'git' -> git repository; 'path' -> path
-    final packageNameOrGitRepoOrPath =
-        readArg('No package name/git repository/path to activate given.');
+    /// 'hosted' -> package name on pub server; 'git' -> git repository url; 'path' -> local path
+    final hostedPackageNameOrGitRepoUrlOrLocalPath =
+        readArg('No package name/git repository url/path to activate given.');
 
     late final Directory packageRootDir;
     switch (source) {
       case 'path':
-        packageRootDir = Directory(packageNameOrGitRepoOrPath);
+        packageRootDir = Directory(hostedPackageNameOrGitRepoUrlOrLocalPath);
         break;
       case 'hosted':
       case 'git':
         final gitPath = args['git-path'] as String?;
         final gitRef = args['git-ref'] as String?;
-
-        // TODO Maybe we should do a `dart pub global list` first to check if
-        // the package is already activated. If it is already activated,
-        // we should at least print a warning because we are altering the
-        // user's environment in a possibly unexpected way by calling
-        // `dart pub global activate/deactivate`.
-        //
-        // In the end, we're calling `dart pub global activate` only to
-        // make sure that the package is available in the cache directory
-        // so we can execute its installation script (bin/install.dart).
-        // The `dart pub cache` command would be more suitable, however it
-        // doesn't offer a way to specify different sources (git/hosted/path)
-        // or version constraints. If it offers these options one day,
-        // we should start using it instead.
-
-        final pubGlobalActivateArgs = [
-          'pub',
-          'global',
-          'activate',
-          '--no-executables',
-          '--source',
-          source,
-          if (hostedUrl != null) hostedUrl.toString(),
-          if (gitPath != null) ...['--git-path', gitPath],
-          if (gitRef != null) ...['--git-ref', gitRef],
-          packageNameOrGitRepoOrPath,
-          // optional version constraint (only valid for hosted source)
-          if (source == 'hosted' && argsRest.isNotEmpty) readArg(),
-          // verbose output so we can parse the git SHA which is a part of
-          // the cache directory for packages downloaded with the git source
-          '-v'
-        ];
+        final hostedVersionConstraint =
+            source == 'hosted' && argsRest.isNotEmpty ? readArg() : null;
         validateNoExtraArgs();
 
-        final progress = dcli.Progress.capture(captureStderr: false);
-        _dartThrowing(pubGlobalActivateArgs, progress: progress);
-
-        // TODO We should definitely do this in a less hacky way
-        // Our goal is to get the cache directory of the package.
-        // We're currently doing this by parsing the package name
-        // and either version (source == 'hosted) or git SHA (source == 'git').
-        // This information is then used to construct the directory names:
-        //
-        // For source == 'hosted':
-        // <pub cache>/hosted/<transformed pub server url>/<package name>-<version>
-        // E.g. /Users/pepe/.pub-cache/hosted/pub.dartlang.org/umbra-0.1.0-dev.4
-        //
-        // For source == 'git': <pub cache>/git/<package name>-<git SHA>
-        // E.g. /Users/pepe/.pub-cache/git/mason-0184b2b833053731878a7f408dd3ed03765a70e8
-        //
-        // It would be great if the pub tool offered a way to directly get
-        // the cache directory instead of using this hacky workaround, maybe we
-        // can raise an issue or open a PR for this on the pub repository.
-        // We should at least add some tests to the pub repository so this
-        // workaround doesn't break in the future.
-
-        // This line contains information about the package name
-        // and resolved version.
-        // - source == 'path':
-        //   Activated bar 1.0.0 at path "/Users/pepe/dev/repos/foo".
-        // - source == 'hosted':
-        //   Activated sidekick_vault 0.5.4.
-        // - source == 'git':
-        //   Activated mason_cli 0.1.0-dev.36 from Git repository "https://github.com/felangel/mason".
-        //
-        // Also see:
-        // https://github.com/dart-lang/pub/blob/master/lib/src/global_packages.dart#L188
-        // https://github.com/dart-lang/pub/blob/master/lib/src/global_packages.dart#L276
-        // https://github.com/dart-lang/pub/blob/master/lib/src/global_packages.dart#L459
-        final activationRegExp =
-            RegExp(r'.*Activated ([a-z_][a-z\d_]*) (\S+)[. ]');
-        final activationInfo = progress.lines
-            .map(activationRegExp.matchAsPrefix)
-            .whereNotNull()
-            .single;
-        final packageName = activationInfo.group(1)!;
-        final packageVersion = activationInfo.group(2)!;
-
-        // The package was only activated to cache it and can be deactivated now
-        _dartThrowing([
-          'pub',
-          'global',
-          'deactivate',
-          packageName,
-        ]);
-
-        switch (source) {
-          case 'hosted':
-            packageRootDir = Directory(
-              join(
-                pubCacheDir,
-                'hosted',
-                _urlToDirectory(hostedUrl?.toString() ?? defaultUrl),
-                '$packageName-$packageVersion',
-              ),
-            );
-            break;
-          case 'git':
-            final gitSHARegExp =
-                RegExp(r'.*git (?:show|checkout) ([a-f0-9]{40})\b');
-            final gitSHA = progress.lines
-                .map(gitSHARegExp.matchAsPrefix)
-                .whereNotNull()
-                .single
-                .group(1)!;
-
-            packageRootDir =
-                Directory(join(pubCacheDir, 'git', '$packageName-$gitSHA'));
-            break;
-          default:
-            throw StateError('unreachable');
-        }
-
+        packageRootDir = _getPackageRootDirForHostedOrGitSource(
+          source: source,
+          hostedPackageNameOrGitRepoUrl:
+              hostedPackageNameOrGitRepoUrlOrLocalPath,
+          gitPath: gitPath,
+          gitRef: gitRef,
+          hostedUrl: hostedUrl,
+          hostedVersionConstraint: hostedVersionConstraint,
+        );
         break;
       default:
         throw StateError('unreachable');
@@ -240,6 +140,122 @@ class AddPluginsCommand extends Command {
 
     // Show errors warning, further instructions
     // TODO shouldn't the bin/install.dart script do this?
+  }
+
+  Directory _getPackageRootDirForHostedOrGitSource({
+    required String source,
+    required String hostedPackageNameOrGitRepoUrl,
+    String? gitPath,
+    String? gitRef,
+    Uri? hostedUrl,
+    String? hostedVersionConstraint,
+  }) {
+    // TODO Maybe we should do a `dart pub global list` first to check if
+    // the package is already activated. If it is already activated,
+    // we should at least print a warning because we are altering the
+    // user's environment in a possibly unexpected way by calling
+    // `dart pub global activate/deactivate`.
+    //
+    // In the end, we're calling `dart pub global activate` only to
+    // make sure that the package is available in the cache directory
+    // so we can execute its installation script (bin/install.dart).
+    // The `dart pub cache` command would be more suitable, however it
+    // doesn't offer a way to specify different sources (git/hosted/path)
+    // or version constraints. If it offers these options one day,
+    // we should start using it instead.
+
+    final pubGlobalActivateArgs = [
+      'pub',
+      'global',
+      'activate',
+      '--no-executables',
+      '--source',
+      source,
+      if (hostedUrl != null) hostedUrl.toString(),
+      if (gitPath != null) ...['--git-path', gitPath],
+      if (gitRef != null) ...['--git-ref', gitRef],
+      hostedPackageNameOrGitRepoUrl,
+      // optional version constraint (only valid for hosted source)
+      if (hostedVersionConstraint != null) hostedVersionConstraint,
+      // verbose output so we can parse the git SHA which is a part of
+      // the cache directory for packages downloaded with the git source
+      '-v'
+    ];
+
+    final progress = dcli.Progress.capture(captureStderr: false);
+    _dartThrowing(pubGlobalActivateArgs, progress: progress);
+
+    // TODO We should definitely do this in a less hacky way
+    // Our goal is to get the cache directory of the package.
+    // We're currently doing this by parsing the package name
+    // and either version (source == 'hosted) or git SHA (source == 'git').
+    // This information is then used to construct the directory names:
+    //
+    // For source == 'hosted':
+    // <pub cache>/hosted/<transformed pub server url>/<package name>-<version>
+    // E.g. /Users/pepe/.pub-cache/hosted/pub.dartlang.org/umbra-0.1.0-dev.4
+    //
+    // For source == 'git': <pub cache>/git/<package name>-<git SHA>
+    // E.g. /Users/pepe/.pub-cache/git/mason-0184b2b833053731878a7f408dd3ed03765a70e8
+    //
+    // It would be great if the pub tool offered a way to directly get
+    // the cache directory instead of using this hacky workaround, maybe we
+    // can raise an issue or open a PR for this on the pub repository.
+    // We should at least add some tests to the pub repository so this
+    // workaround doesn't break in the future.
+
+    // This line contains information about the package name
+    // and resolved version.
+    // - source == 'path':
+    //   Activated bar 1.0.0 at path "/Users/pepe/dev/repos/foo".
+    // - source == 'hosted':
+    //   Activated sidekick_vault 0.5.4.
+    // - source == 'git':
+    //   Activated mason_cli 0.1.0-dev.36 from Git repository "https://github.com/felangel/mason".
+    //
+    // Also see:
+    // https://github.com/dart-lang/pub/blob/master/lib/src/global_packages.dart#L188
+    // https://github.com/dart-lang/pub/blob/master/lib/src/global_packages.dart#L276
+    // https://github.com/dart-lang/pub/blob/master/lib/src/global_packages.dart#L459
+    final activationRegExp = RegExp(r'.*Activated ([a-z_][a-z\d_]*) (\S+)[. ]');
+    final activationInfo = progress.lines
+        .map(activationRegExp.matchAsPrefix)
+        .whereNotNull()
+        .single;
+    final packageName = activationInfo.group(1)!;
+    final packageVersion = activationInfo.group(2)!;
+
+    // The package was only activated to cache it and can be deactivated now
+    _dartThrowing([
+      'pub',
+      'global',
+      'deactivate',
+      packageName,
+    ]);
+
+    switch (source) {
+      case 'hosted':
+        return Directory(
+          join(
+            pubCacheDir,
+            'hosted',
+            _urlToDirectory(hostedUrl?.toString() ?? defaultUrl),
+            '$packageName-$packageVersion',
+          ),
+        );
+      case 'git':
+        final gitSHARegExp =
+            RegExp(r'.*git (?:show|checkout) ([a-f0-9]{40})\b');
+        final gitSHA = progress.lines
+            .map(gitSHARegExp.matchAsPrefix)
+            .whereNotNull()
+            .single
+            .group(1)!;
+
+        return Directory(join(pubCacheDir, 'git', '$packageName-$gitSHA'));
+      default:
+        throw StateError('unreachable');
+    }
   }
 }
 
