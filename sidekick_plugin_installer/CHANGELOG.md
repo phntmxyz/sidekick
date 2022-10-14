31cbece3d00a7ab4aa8ad3c2d7668c8e18499709
Pascal Welsch <pascal@phntm.xyz>
2022-10-14 21:07:44 +0200

    Add plugin_installer package
    Useful snippets for plugin installers

---
+++ sidekick_plugin_installer/CHANGELOG.md
@@ -0,0 +1,6 @@
+## 0.1.0
+
+- New: `addImport`
+- New: `pubAddDependency`
+- New: `pubGet`
+- New: `registerPlugin`
---
+++ sidekick_plugin_installer/lib/src/pub_get.dart
@@ -0,0 +1,9 @@
+import 'package:sidekick_core/sidekick_core.dart';
+
+/// Runs pub get on [package]
+void pubGet(DartPackage package) {
+  sidekickDartRuntime.dart(
+    ['pub', 'get'],
+    workingDirectory: package.root,
+  );
+}
