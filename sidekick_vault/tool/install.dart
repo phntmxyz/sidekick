30dfe6a40ce87c86ad4862a66eb8f298853296be
Pascal Welsch <pascal@phntm.xyz>
2022-10-14 22:42:28 +0200

    Fix cliName

--- sidekick_vault/tool/install.dart
+++ sidekick_vault/tool/install.dart
@@ -1,4 +1,5 @@
-import 'package:sidekick_core/sidekick_core.dart';
+import 'package:sidekick_core/sidekick_core.dart'
+    hide cliName, repository, mainProject;
 import 'package:sidekick_plugin_installer/sidekick_plugin_installer.dart';
 
 Future<void> main(List<String> args) async {
@@ -18,7 +19,7 @@ Future<void> main(List<String> args) async {
 
 void _writeVaultFile(SidekickPackage package) {
   final vaultFile = package.root.file('lib/src/vault.dart');
-  // final cliName = package.cliName;
+  final cliName = package.cliName;
   vaultFile.writeAsStringSync('''
 import 'package:sidekick_vault/sidekick_vault.dart';
 
