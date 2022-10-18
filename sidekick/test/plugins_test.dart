1f059ddc0070a7a8b532a4f8e006328584397736
Giuseppe Cianci <giuseppe.cianci97@gmail.com>
2022-10-18 17:50:19 +0200

    format

--- sidekick/test/plugins_test.dart
+++ sidekick/test/plugins_test.dart
@@ -35,95 +35,98 @@ void main() {
     }
   });
 
-  group('plugins install executes fine', () {
-    test(
-      'with default hosted source',
-      () async {
-        final dashProcess =
-            await startDashProcess(['plugins', 'install', 'sidekick_vault']);
-        printOnFailure(await dashProcess.stdoutStream().join('\n'));
-        printOnFailure(await dashProcess.stderrStream().join('\n'));
-        dashProcess.shouldExit(0);
-      },
-      timeout: const Timeout(Duration(minutes: 5)),
-      skip: 'Wait for first plugin to be published',
-    );
+  group(
+    'plugins install executes fine',
+    () {
+      test(
+        'with default hosted source',
+        () async {
+          final dashProcess =
+              await startDashProcess(['plugins', 'install', 'sidekick_vault']);
+          printOnFailure(await dashProcess.stdoutStream().join('\n'));
+          printOnFailure(await dashProcess.stderrStream().join('\n'));
+          dashProcess.shouldExit(0);
+        },
+        timeout: const Timeout(Duration(minutes: 5)),
+        skip: 'Wait for first plugin to be published',
+      );
 
-    test(
-      'with custom hosted source',
-      () async {
-        final dashProcess = await startDashProcess([
-          'plugins',
-          'install',
-          '--hosted-url',
-          'https://pub.flutter-io.cn/',
-          'sidekick_vault',
-        ]);
-        printOnFailure(await dashProcess.stdoutStream().join('\n'));
-        printOnFailure(await dashProcess.stderrStream().join('\n'));
-        dashProcess.shouldExit(0);
-      },
-      timeout: const Timeout(Duration(minutes: 5)),
-      skip: 'Wait for first plugin to be published',
-    );
+      test(
+        'with custom hosted source',
+        () async {
+          final dashProcess = await startDashProcess([
+            'plugins',
+            'install',
+            '--hosted-url',
+            'https://pub.flutter-io.cn/',
+            'sidekick_vault',
+          ]);
+          printOnFailure(await dashProcess.stdoutStream().join('\n'));
+          printOnFailure(await dashProcess.stderrStream().join('\n'));
+          dashProcess.shouldExit(0);
+        },
+        timeout: const Timeout(Duration(minutes: 5)),
+        skip: 'Wait for first plugin to be published',
+      );
 
-    test(
-      'with git source',
-      () async {
-        final dashProcess = await startDashProcess([
-          'plugins',
-          'install',
-          '--source',
-          'git',
-          '--git-path',
-          'packages/umbra_cli',
-          'https://github.com/wolfenrain/umbra',
-        ]);
-        printOnFailure(await dashProcess.stdoutStream().join('\n'));
-        printOnFailure(await dashProcess.stderrStream().join('\n'));
-        dashProcess.shouldExit(0);
-      },
-      timeout: const Timeout(Duration(minutes: 5)),
-      skip: 'Wait for first plugin to be published',
-    );
+      test(
+        'with git source',
+        () async {
+          final dashProcess = await startDashProcess([
+            'plugins',
+            'install',
+            '--source',
+            'git',
+            '--git-path',
+            'packages/umbra_cli',
+            'https://github.com/wolfenrain/umbra',
+          ]);
+          printOnFailure(await dashProcess.stdoutStream().join('\n'));
+          printOnFailure(await dashProcess.stderrStream().join('\n'));
+          dashProcess.shouldExit(0);
+        },
+        timeout: const Timeout(Duration(minutes: 5)),
+        skip: 'Wait for first plugin to be published',
+      );
 
-    test(
-      'with local source',
-      () async {
-        // DartScript.self.pathToScriptDirectory has a bug and returns
-        // /Users/pepe/dev/repos/sidekick/sidekick/async
-        // instead of
-        // /Users/pepe/dev/repos/sidekick/sidekick
-        // so I have to parse the correct path
-        final sidekickPath = RegExp(r'(^.*[/\\]sidekick[/\\]sidekick)')
-            .matchAsPrefix(DartScript.self.pathToScriptDirectory)!
-            .group(1)!;
+      test(
+        'with local source',
+        () async {
+          // DartScript.self.pathToScriptDirectory has a bug and returns
+          // /Users/pepe/dev/repos/sidekick/sidekick/async
+          // instead of
+          // /Users/pepe/dev/repos/sidekick/sidekick
+          // so I have to parse the correct path
+          final sidekickPath = RegExp(r'(^.*[/\\]sidekick[/\\]sidekick)')
+              .matchAsPrefix(DartScript.self.pathToScriptDirectory)!
+              .group(1)!;
 
-        final pluginPath = Directory(sidekickPath)
-            .directory('test/templates/minimal_sidekick_plugin')
-            .path;
+          final pluginPath = Directory(sidekickPath)
+              .directory('test/templates/minimal_sidekick_plugin')
+              .path;
 
-        final dashProcess = await startDashProcess([
-          'plugins',
-          'install',
-          '--source',
-          'path',
-          pluginPath,
-        ]);
-        printOnFailure(await dashProcess.stdoutStream().join('\n'));
-        printOnFailure(await dashProcess.stderrStream().join('\n'));
-        dashProcess.shouldExit(0);
+          final dashProcess = await startDashProcess([
+            'plugins',
+            'install',
+            '--source',
+            'path',
+            pluginPath,
+          ]);
+          printOnFailure(await dashProcess.stdoutStream().join('\n'));
+          printOnFailure(await dashProcess.stderrStream().join('\n'));
+          dashProcess.shouldExit(0);
 
-        final pluginProcess = await startDashProcess(
-          ['minimal-sidekick-plugin'],
-        );
-        printOnFailure(await pluginProcess.stdoutStream().join('\n'));
-        printOnFailure(await pluginProcess.stderrStream().join('\n'));
-        pluginProcess.shouldExit(0);
-      },
-      timeout: const Timeout(Duration(minutes: 5)),
-    );
-  });
+          final pluginProcess = await startDashProcess(
+            ['minimal-sidekick-plugin'],
+          );
+          printOnFailure(await pluginProcess.stdoutStream().join('\n'));
+          printOnFailure(await pluginProcess.stderrStream().join('\n'));
+          pluginProcess.shouldExit(0);
+        },
+        timeout: const Timeout(Duration(minutes: 5)),
+      );
+    },
+  );
 
   group('e2e: plugins create, plugins install, run plugin', () {
     // TODO use CreatePluginCommand.templates.keys instead when new version of sidekick_core is published
