a5c1c05d994f169dee67156b4e26798acd2e874a
Giuseppe Cianci <giuseppe.cianci97@gmail.com>
2022-10-18 12:09:18 +0200

    remove debugging code

--- sidekick_core/test/commands/plugins/create_plugin_command_test.dart
+++ sidekick_core/test/commands/plugins/create_plugin_command_test.dart
@@ -9,7 +9,7 @@ void main() {
 
     Future<String> generatePlugin(String template) async {
       final tempDir = Directory.systemTemp.createTempSync();
-      //addTearDown(() => tempDir.deleteSync(recursive: true));
+      addTearDown(() => tempDir.deleteSync(recursive: true));
 
       final runner = initializeSidekick(name: 'dash');
       runner.addCommand(CreatePluginCommand());
@@ -25,11 +25,6 @@ void main() {
 
         run('dart pub get', workingDirectory: pluginPath);
         run('dart analyze', workingDirectory: pluginPath);
-        
-        run('git init', workingDirectory: pluginPath);
-        run('git add .', workingDirectory: pluginPath);
-        run('git commit -am "initial"', workingDirectory: pluginPath);
-
         run('dart format --set-exit-if-changed $pluginPath');
       });
     }
