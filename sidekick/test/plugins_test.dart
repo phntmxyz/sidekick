151d4c2bc1c6d5389eb97253f7f384e542f15282
Pascal Welsch <pascal@phntm.xyz>
2022-10-19 21:43:45 +0200

    Make test names unique, allowing running them individually

--- sidekick/test/plugins_test.dart
+++ sidekick/test/plugins_test.dart
@@ -19,8 +19,8 @@ void main() {
       workingDirectory: projectRoot.path,
     );
 
-    process.stdoutStream().listen(printOnFailure);
-    process.stderrStream().listen(printOnFailure);
+    process.stdoutStream().listen(print);
+    process.stderrStream().listen(print);
     await process.shouldExit(0);
   }
 
@@ -41,144 +41,135 @@ void main() {
     }
   });
 
-  group(
-    'plugins install executes fine',
-    () {
-      test(
-        'with default hosted source',
-        () async {
-          await runDashProcess(['plugins', 'install', 'sidekick_vault']);
-        },
-        timeout: const Timeout(Duration(minutes: 5)),
-        skip: 'Wait for first plugin to be published',
-      );
+  group('plugins install executes fine', () {
+    test(
+      'with default hosted source',
+      () async {
+        await runDashProcess(['plugins', 'install', 'sidekick_vault']);
+      },
+      timeout: const Timeout(Duration(minutes: 5)),
+      skip: 'Wait for first plugin to be published',
+    );
 
-      test(
-        'with custom hosted source',
-        () async {
-          await runDashProcess([
-            'plugins',
-            'install',
-            '--hosted-url',
-            'https://pub.flutter-io.cn/',
-            'sidekick_vault',
-          ]);
-        },
-        timeout: const Timeout(Duration(minutes: 5)),
-        skip: 'Wait for first plugin to be published',
-      );
+    test(
+      'with custom hosted source',
+      () async {
+        await runDashProcess([
+          'plugins',
+          'install',
+          '--hosted-url',
+          'https://pub.flutter-io.cn/',
+          'sidekick_vault',
+        ]);
+      },
+      timeout: const Timeout(Duration(minutes: 5)),
+      skip: 'Wait for first plugin to be published',
+    );
 
-      test(
-        'with git source',
-        () async {
-          await runDashProcess([
-            'plugins',
-            'install',
-            '--source',
-            'git',
-            '--git-path',
-            'packages/umbra_cli',
-            'https://github.com/wolfenrain/umbra',
-          ]);
-        },
-        timeout: const Timeout(Duration(minutes: 5)),
-        skip: 'Wait for first plugin to be published',
-      );
+    test(
+      'with git source',
+      () async {
+        await runDashProcess([
+          'plugins',
+          'install',
+          '--source',
+          'git',
+          '--git-path',
+          'packages/umbra_cli',
+          'https://github.com/wolfenrain/umbra',
+        ]);
+      },
+      timeout: const Timeout(Duration(minutes: 5)),
+      skip: 'Wait for first plugin to be published',
+    );
 
-      test(
-        'with local source',
-        () async {
-          // DartScript.self.pathToScriptDirectory has a bug and returns
-          // /Users/pepe/dev/repos/sidekick/sidekick/async
-          // instead of
-          // /Users/pepe/dev/repos/sidekick/sidekick
-          // so I have to parse the correct path
-          final sidekickPath = RegExp(r'(^.*[/\\]sidekick[/\\]sidekick)')
-              .matchAsPrefix(DartScript.self.pathToScriptDirectory)!
-              .group(1)!;
+    test(
+      'with local source',
+      () async {
+        // DartScript.self.pathToScriptDirectory has a bug and returns
+        // /Users/pepe/dev/repos/sidekick/sidekick/async
+        // instead of
+        // /Users/pepe/dev/repos/sidekick/sidekick
+        // so I have to parse the correct path
+        final sidekickPath = RegExp(r'(^.*[/\\]sidekick[/\\]sidekick)')
+            .matchAsPrefix(DartScript.self.pathToScriptDirectory)!
+            .group(1)!;
 
-          final pluginPath = Directory(sidekickPath)
-              .directory('test/templates/minimal_sidekick_plugin')
-              .path;
+        final pluginPath = Directory(sidekickPath)
+            .directory('test/templates/minimal_sidekick_plugin')
+            .path;
 
-          await runDashProcess([
-            'plugins',
-            'install',
-            '--source',
-            'path',
-            pluginPath,
-          ]);
+        await runDashProcess([
+          'plugins',
+          'install',
+          '--source',
+          'path',
+          pluginPath,
+        ]);
 
-          await runDashProcess(
-            ['minimal-sidekick-plugin'],
-          );
-        },
-        timeout: const Timeout(Duration(minutes: 5)),
-      );
-    },
-  );
+        await runDashProcess(
+          ['minimal-sidekick-plugin'],
+        );
+      },
+      timeout: const Timeout(Duration(minutes: 5)),
+    );
+  });
 
-  group('plugins create generates valid plugin code', () {
-    // TODO: use CreatePluginCommand.templates.keys instead (import 'package:sidekick_core/src/commands/plugins/create_plugin_command.dart';)
-    const templates = [
-      'install-only',
-      'shared-command',
-      'shared-code',
-    ];
-    for (final template in templates) {
-      test('for template $template', () async {
+  // TODO: use CreatePluginCommand.templates.keys instead (import 'package:sidekick_core/src/commands/plugins/create_plugin_command.dart';)
+  const templates = [
+    'install-only',
+    'shared-command',
+    'shared-code',
+  ];
+
+  for (final template in templates) {
+    test('plugin template $template generates valid plugin code', () async {
+      await runDashProcess([
+        'plugins',
+        'create',
+        '-t',
+        template,
+        '-n',
+        'generated_plugin',
+        projectRoot.path,
+      ]);
+
+      final pluginPath = projectRoot.directory('generated_plugin').path;
+
+      run('dart pub get', workingDirectory: pluginPath);
+      run('dart analyze', workingDirectory: pluginPath);
+      run('dart format --set-exit-if-changed $pluginPath');
+    });
+  }
+
+  for (final template in templates) {
+    test(
+      'plugin e2e $template: create, install, run',
+      () async {
         await runDashProcess([
           'plugins',
           'create',
           '-t',
           template,
           '-n',
-          'generated_plugin',
-          projectRoot.path,
+          template.snakeCase,
         ]);
 
-        final pluginPath = projectRoot.directory('generated_plugin').path;
-
-        run('dart pub get', workingDirectory: pluginPath);
-        run('dart analyze', workingDirectory: pluginPath);
-        run('dart format --set-exit-if-changed $pluginPath');
-      });
-    }
-  });
-
-  group('e2e: plugins create, plugins install, run plugin', () {
-    // TODO use CreatePluginCommand.templates.keys instead when new version of sidekick_core is published
-    const templates = ['install-only', 'shared-command', 'shared-code'];
-
-    test(
-      'for all templates',
-      () async {
-        for (final template in templates) {
-          await runDashProcess([
+        await runDashProcess(
+          [
             'plugins',
-            'create',
-            '-t',
-            template,
-            '-n',
+            'install',
+            '-s',
+            'path',
             template.snakeCase,
-          ]);
+          ],
+        );
 
-          await runDashProcess(
-            [
-              'plugins',
-              'install',
-              '-s',
-              'path',
-              template.snakeCase,
-            ],
-          );
-
-          await runDashProcess(
-            [template.paramCase],
-          );
-        }
+        await runDashProcess(
+          [template.paramCase],
+        );
       },
       timeout: const Timeout(Duration(minutes: 5)),
     );
-  });
+  }
 }
