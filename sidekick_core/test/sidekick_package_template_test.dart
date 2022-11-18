219e157f724e567059c794efafd05225641d698a
Giuseppe Cianci <giuseppe.cianci97@gmail.com>
2022-11-18 15:04:42 +0100

    replace titleCase with pascalCase

--- sidekick_core/test/sidekick_package_template_test.dart
+++ sidekick_core/test/sidekick_package_template_test.dart
@@ -12,7 +12,7 @@ void main() {
   });
 
   tearDown(() {
-    //tempDir.deleteSync(recursive: true);
+    tempDir.deleteSync(recursive: true);
   });
 
   for (final cliName in const ['dashi', 'foo_bar']) {
