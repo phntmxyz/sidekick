83bb0ba6938647ff30a5a14d59c312d7c9605c42
Giuseppe Cianci <giuseppe.cianci97@gmail.com>
2022-11-21 15:51:15 +0200

    new addDependency function which selects correct dependency type; inject plugin arguments into env

--- sidekick_plugin_installer/lib/src/plugin_context.dart
+++ sidekick_plugin_installer/lib/src/plugin_context.dart
@@ -26,31 +26,51 @@ class PluginContext {
 
   static SidekickPackage? _sidekickPackage;
 
-  /// This is the plugin package that is getting installed from local source
+  /// Name of the plugin package to be installed
   ///
-  /// Plugin installer might need to know the local location during development,
-  /// to link the plugin dependency to a local path, instead of pub.dev where
-  /// the plugin is not yet published. (See [pubAddLocalDependency])
-  ///
-  /// Returns `null` when the plugin is not installed from local source, but
-  /// from a remote source (pub or git)
-  static DartPackage? get localPlugin {
-    if (_localPlugin == null) {
-      final path = env['SIDEKICK_LOCAL_PLUGIN_PATH'];
-      if (path == null) {
-        return null;
-      }
-      final dir = Directory(path);
-      if (!dir.existsSync()) {
-        throw "Directory at ${dir.absolute.path} (env.SIDEKICK_LOCAL_PLUGIN_PATH) does not exist";
-      }
-      _localPlugin = DartPackage.fromDirectory(dir);
-      if (_localPlugin == null) {
-        throw "Directory at ${dir.absolute.path} (env.SIDEKICK_LOCAL_PLUGIN_PATH) is not a dart package";
-      }
-    }
-    return _localPlugin;
-  }
+  /// {@macro installer_parameter}
+  static String get name => env['SIDEKICK_PLUGIN_NAME']!;
 
-  static DartPackage? _localPlugin;
+  /// Version constraint of the plugin package to be installed
+  ///
+  /// {@macro installer_parameter}
+  static String? get versionConstraint =>
+      env['SIDEKICK_PLUGIN_VERSION_CONSTRAINT'];
+
+  /// Path to plugin package which is getting installed from local source
+  ///
+  /// {@macro installer_parameter}
+  static String? get localPath => env['SIDEKICK_PLUGIN_LOCAL_PATH'];
+
+  /// {@macro installer_parameter}
+  ///
+  /// When null, the default server (pub.dev) is used
+  ///
+  /// URL of the pub server to install the plugin package from
+  static String? get hostedUrl => env['SIDEKICK_PLUGIN_HOSTED_URL'];
+
+  /// Git URL of the plugin package
+  ///
+  /// {@macro installer_parameter}
+  static String? get gitUrl => env['SIDEKICK_PLUGIN_GIT_URL'];
+
+  /// Git branch or commit to be retrieved
+  ///
+  /// {@macro installer_parameter}
+  static String? get gitRef => env['SIDEKICK_PLUGIN_GIT_REF'];
+
+  /// Path of git package in repository
+  ///
+  /// {@macro installer_parameter}
+  static String? get gitPath => env['SIDEKICK_PLUGIN_GIT_PATH'];
 }
+
+/// {@template installer_parameter}
+/// This is a parameter for installing a plugin package in a sidekick CLI
+///
+/// The plugin installer needs to know the location of the plugin package
+/// which can either be a local path, a remote pub server, or a remote git
+/// repository. See [addDependency]
+///
+/// Returns `null` when the plugin is installed from another source
+/// {@endtemplate}
