e6105d2d26a8de7253ff0b3c658a1a0129611403
Giuseppe Cianci <giuseppe.cianci97@gmail.com>
2022-11-23 16:19:49 +0200

    deprecate instead of removing

---
+++ sidekick_plugin_installer/lib/src/pub_add_dependency.dart
@@ -0,0 +1,23 @@
+import 'package:sidekick_core/sidekick_core.dart';
+
+/// This function is deprecated. Use [addDependency] instead
+///
+/// Adds the dependency to [package]
+@Deprecated('Use `addDependency` instead.')
+void pubAddDependency(
+  DartPackage package,
+  String dependency, {
+  String? version,
+}) {
+  sidekickDartRuntime.dart(
+    ['pub', 'remove', dependency],
+    workingDirectory: package.root,
+    progress: Progress.devNull(),
+  );
+
+  sidekickDartRuntime.dart(
+    ['pub', 'add', dependency, if (version != null) version],
+    workingDirectory: package.root,
+    progress: Progress.printStdErr(),
+  );
+}
