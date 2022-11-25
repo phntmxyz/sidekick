0dc1f84aac3c09c7c67e81aa9bd5e7fb248205ec
Pascal Welsch <pascal@phntm.xyz>
2022-11-25 19:56:56 +0100

    Export deprecated methods again

--- sidekick_plugin_installer/lib/sidekick_plugin_installer.dart
+++ sidekick_plugin_installer/lib/sidekick_plugin_installer.dart
@@ -3,5 +3,7 @@ library sidekick_plugin_installer;
 export 'package:sidekick_plugin_installer/src/add_dependency.dart';
 export 'package:sidekick_plugin_installer/src/add_import.dart';
 export 'package:sidekick_plugin_installer/src/plugin_context.dart';
+export 'package:sidekick_plugin_installer/src/pub_add_dependency.dart';
+export 'package:sidekick_plugin_installer/src/pub_add_local_dependency.dart';
 export 'package:sidekick_plugin_installer/src/pub_get.dart';
 export 'package:sidekick_plugin_installer/src/register_plugin.dart';
