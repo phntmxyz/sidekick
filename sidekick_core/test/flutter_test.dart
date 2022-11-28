e8a34ef1ea3c951bde94571f2663b7943c0d60de
Giuseppe Cianci <giuseppe.cianci97@gmail.com>
2022-11-28 11:59:31 +0200

    systemFlutterSdk: check for env.FLUTTER_ROOT

--- sidekick_core/test/flutter_test.dart
+++ sidekick_core/test/flutter_test.dart
@@ -4,27 +4,39 @@ import 'package:sidekick_core/sidekick_core.dart';
 import 'package:test/test.dart';
 
 void main() {
-  group('systemFlutterSdkPath', () {
-    test('returns null when flutter is not on path', () {
-      final whichPath =
-          start('which which', progress: Progress.capture()).firstLine!;
-      PATH.where((p) => !whichPath.startsWith(p)).forEach(env.removeFromPATH);
+  final tempDir = Directory.systemTemp.createTempSync();
+  final fakeFlutter = tempDir.file('fake_flutter/bin/flutter')
+    ..createSync(recursive: true);
+  chmod(fakeFlutter.path, permission: '755');
 
+  setUp(() {
+    final whichPath =
+        start('which which', progress: Progress.capture()).firstLine!;
+    PATH.where((p) => !whichPath.startsWith(p)).forEach(env.removeFromPATH);
+    env['FLUTTER_ROOT'] = null;
+  });
+
+  tearDownAll(() {
+    tempDir.deleteSync(recursive: true);
+  });
+
+  group('systemFlutterSdkPath', () {
+    test('returns null when flutter is not on path or env.FLUTTER_ROOT', () {
       expect(systemFlutterSdkPath(), null);
     });
 
-    test('returns correct path when flutter is on path', () {
-      final whichPath =
-          start('which which', progress: Progress.capture()).firstLine!;
-      PATH.where((p) => !whichPath.startsWith(p)).forEach(env.removeFromPATH);
-
-      final tempDir = Directory.systemTemp.createTempSync();
-      addTearDown(() => tempDir.deleteSync(recursive: true));
-
-      final fakeFlutter = tempDir.file('fake_flutter/bin/flutter')
-        ..createSync(recursive: true);
-      chmod(fakeFlutter.path, permission: '755');
+    test('prioritizes flutter from path over env.FLUTTER_ROOT', () {
+      env['FLUTTER_ROOT'] = '/foo/flutter';
       env.appendToPATH(fakeFlutter.parent.path);
+
+      expect(
+        systemFlutterSdkPath(),
+        tempDir.directory('fake_flutter').resolveSymbolicLinksSync(),
+      );
+    });
+
+    test('returns correct path when env.FLUTTER_ROOT is defined', () {
+      env['FLUTTER_ROOT'] = fakeFlutter.path;
 
       expect(
         systemFlutterSdkPath(),
