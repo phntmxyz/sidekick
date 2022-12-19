ac649959171ffe0e1e63205d7e0df73e8a204aaa
Giuseppe Cianci <giuseppe.cianci97@gmail.com>
2022-12-19 17:32:16 +0100

    fix very_good_cli installation

--- sidekick/test/util/cli_runner.dart
+++ sidekick/test/util/cli_runner.dart
@@ -3,6 +3,7 @@ import 'dart:async';
 import 'package:dcli/dcli.dart';
 import 'package:sidekick_core/sidekick_core.dart';
 import 'package:sidekick_test/sidekick_test.dart';
+import 'package:test/test.dart';
 import 'package:test_process/test_process.dart';
 
 /// Deletes cached versions of the sidekick executable and sidekick CLI
@@ -20,7 +21,7 @@ void tearDownSidekickCache() {
 /// sidekick_core dependency is linked to the local version
 R withSidekickCli<R>(R Function(SidekickCli cli) callback) {
   final copy = Directory.systemTemp.createTempSync();
-  // addTearDown(() => copy.deleteSync(recursive: true));
+  addTearDown(() => copy.deleteSync(recursive: true));
   waitForEx(_cachedSidekickCli.root.copyRecursively(copy));
 
   overrideSidekickCoreWithLocalPath(copy.directory('packages/dashi_sidekick'));
