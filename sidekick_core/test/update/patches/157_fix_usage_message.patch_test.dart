f780d83103e00e974071e55bf6dc9b71b26ba012
Giuseppe Cianci <giuseppe.cianci97@gmail.com>
2023-01-03 12:39:32 +0100

    refactor test

--- sidekick_core/test/update/patches/157_fix_usage_message.patch_test.dart
+++ sidekick_core/test/update/patches/157_fix_usage_message.patch_test.dart
@@ -4,28 +4,47 @@ import 'package:sidekick_core/src/update/patches/157_fix_usage_message.patch.dar
 import 'package:test/test.dart';
 
 void main() {
-  test('patch 157 works', () async {
-    final tempDir = Directory.systemTemp.createTempSync();
-    'git init'.start(workingDirectory: tempDir.path);
-    env['SIDEKICK_PACKAGE_HOME'] = tempDir.absolute.path;
-    addTearDown(() {
-      tempDir.deleteSync(recursive: true);
-      env['SIDEKICK_PACKAGE_HOME'] = null;
+  for (final testCase in _testCases) {
+    test('patch 157 works (${testCase.name})', () async {
+      final tempDir = Directory.systemTemp.createTempSync();
+      'git init'.start(workingDirectory: tempDir.path);
+      env['SIDEKICK_PACKAGE_HOME'] = tempDir.absolute.path;
+      addTearDown(() {
+        tempDir.deleteSync(recursive: true);
+        env['SIDEKICK_PACKAGE_HOME'] = null;
+      });
+      tempDir.file('pubspec.yaml').writeAsStringSync('name: test_sidekick');
+      final cliMainFile = tempDir.file('lib/test_sidekick.dart')
+        ..createSync(recursive: true)
+        ..writeAsStringSync(testCase.fileContentBefore);
+
+      await migrate(
+        from: Version(0, 13, 1),
+        to: Version(0, 13, 2),
+        migrations: [fixUsageMessagePatch157],
+      );
+
+      expect(cliMainFile.readAsStringSync(), testCase.fileContentAfter);
     });
-    tempDir.file('pubspec.yaml').writeAsStringSync('name: test_sidekick');
-    final cliMainFile = tempDir.file('lib/test_sidekick.dart')
-      ..createSync(recursive: true)
-      ..writeAsStringSync('''
-import 'dart:async';
+  }
+}
 
-import 'package:sidekick_core/sidekick_core.dart';
-import 'package:test_sidekick/src/commands/bump_version_command.dart';
-import 'package:test_sidekick/src/release/sidekick_bundled_version_bump.dart';
-import 'package:test_sidekick/src/release/sidekick_core_bundled_version_bump.dart';
-import 'package:test_sidekick/src/test_project.dart';
+class _TestCase {
+  const _TestCase({
+    required this.name,
+    required this.fileContentBefore,
+    required this.fileContentAfter,
+  });
 
-late TestProject testProject;
+  final String name;
+  final String fileContentBefore;
+  final String fileContentAfter;
+}
 
+const _testCases = [
+  _TestCase(
+    name: 'default case',
+    fileContentBefore: '''
 Future<void> runTest(List<String> args) async {
   final runner = initializeSidekick(
     name: 'test',
@@ -49,25 +68,8 @@ Future<void> runTest(List<String> args) async {
     exit(64); // usage error
   }
 }
-''');
-
-    await migrate(
-      from: Version(0, 13, 1),
-      to: Version(0, 13, 2),
-      migrations: [fixUsageMessagePatch157],
-    );
-
-    expect(cliMainFile.readAsStringSync(), '''
-import 'dart:async';
-
-import 'package:sidekick_core/sidekick_core.dart';
-import 'package:test_sidekick/src/commands/bump_version_command.dart';
-import 'package:test_sidekick/src/release/sidekick_bundled_version_bump.dart';
-import 'package:test_sidekick/src/release/sidekick_core_bundled_version_bump.dart';
-import 'package:test_sidekick/src/test_project.dart';
-
-late TestProject testProject;
-
+''',
+    fileContentAfter: '''
 Future<void> runTest(List<String> args) async {
   final runner = initializeSidekick(
     name: 'test',
@@ -86,6 +88,49 @@ Future<void> runTest(List<String> args) async {
     exit(64); // usage error
   }
 }
-''');
-  });
+''',
+  ),
+  _TestCase(
+    name: 'patch was already applied',
+    fileContentBefore: '''
+Future<void> runTest(List<String> args) async {
+  final runner = initializeSidekick(
+    name: 'test',
+    flutterSdkPath: systemFlutterSdkPath(),
+  );
+
+  testProject = TestProject(runner.repository.root);
+  runner
+    ..addCommand(DartCommand())
+    ..addCommand(SidekickCommand());
+
+  try {
+    return await runner.run(args);
+  } on UsageException catch (e) {
+    print(e);
+    exit(64); // usage error
+  }
 }
+''',
+    fileContentAfter: '''
+Future<void> runTest(List<String> args) async {
+  final runner = initializeSidekick(
+    name: 'test',
+    flutterSdkPath: systemFlutterSdkPath(),
+  );
+
+  testProject = TestProject(runner.repository.root);
+  runner
+    ..addCommand(DartCommand())
+    ..addCommand(SidekickCommand());
+
+  try {
+    return await runner.run(args);
+  } on UsageException catch (e) {
+    print(e);
+    exit(64); // usage error
+  }
+}
+''',
+  ),
+];
