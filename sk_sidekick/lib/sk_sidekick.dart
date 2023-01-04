dcc4c3ca4c3eb57d5c77c4e9d9442cead005a40b
Giuseppe Cianci <giuseppe.cianci97@gmail.com>
2023-01-04 23:57:49 +0100

    Add ReleaseCommand

--- sk_sidekick/lib/sk_sidekick.dart
+++ sk_sidekick/lib/sk_sidekick.dart
@@ -3,6 +3,7 @@ import 'dart:async';
 import 'package:sidekick_core/sidekick_core.dart';
 import 'package:sk_sidekick/src/commands/bump_version_command.dart';
 import 'package:sk_sidekick/src/commands/lock_dependencies_command.dart';
+import 'package:sk_sidekick/src/commands/release_command.dart';
 import 'package:sk_sidekick/src/release/sidekick_bundled_version_bump.dart';
 import 'package:sk_sidekick/src/release/sidekick_core_bundled_version_bump.dart';
 import 'package:sk_sidekick/src/sk_project.dart';
@@ -22,6 +23,7 @@ Future<void> runSk(List<String> args) async {
     ..addCommand(DepsCommand(exclude: [...testPackages]))
     ..addCommand(DartAnalyzeCommand())
     ..addCommand(LockDependenciesCommand())
+    ..addCommand(ReleaseCommand())
     ..addCommand(
       BumpVersionCommand()
         ..addModification(sidekickCoreBundledVersionBump)
