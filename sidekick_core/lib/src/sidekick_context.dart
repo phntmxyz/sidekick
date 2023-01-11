9cf5bba2d61585c46655218c3a3705d1c2b7d9cb
Pascal Welsch <pascal@phntm.xyz>
2023-01-11 10:30:51 +0100

    Docs

--- sidekick_core/lib/src/sidekick_context.dart
+++ sidekick_core/lib/src/sidekick_context.dart
@@ -5,16 +5,19 @@ import 'package:sidekick_core/sidekick_core.dart';
 import 'package:sidekick_core/sidekick_core.dart' as core;
 import 'package:sidekick_core/src/sidekick_package.dart';
 
-/// Environment variable containing the location of the shell `entrypoint`, when
+/// Environment variable containing the location of the shell `entryPoint`, when
 /// executing the sidekick CLI with the shell entrypoint
 ///
 /// May be not set when debugging where the CLI is executed directly on the
 /// DartVM by calling `dart bin/main.dart` without the entrypoint
-const String _envEntrypointHome = 'SIDEKICK_ENTRYPOINT_HOME';
+const String _envEntryPointHome = 'SIDEKICK_ENTRYPOINT_HOME';
 
 /// Environment variable containing the location of the dart package of this
 /// sidekick CLI. It contains the source code and tool scripts of this sidekick
 /// CLI.
+///
+/// SIDEKICK_PACKAGE_HOME is set by the `tool/run.sh` script and always called
+/// when executed via `entryPoint`
 const String _envPackageHome = 'SIDEKICK_PACKAGE_HOME';
 
 class SidekickContext {
@@ -60,13 +63,14 @@ class SidekickContext {
   static SidekickPackage get sidekickPackage {
     final injectedPackageHome = env[_envPackageHome];
     if (injectedPackageHome != null && injectedPackageHome.isNotBlank) {
+      // When called via entryPoint, immediately return the package
       return SidekickPackage.fromDirectory(Directory(injectedPackageHome))!;
     }
 
     final script = File(DartScript.self.pathToScript);
     final scriptPath = script.uri.path;
 
-    // When CLI is run with compiled entrypoint: /Users/pepe/repos/sidekick/sk_sidekick/build/cli.exe
+    // When CLI is run with compiled entryPoint: /Users/pepe/repos/sidekick/sk_sidekick/build/cli.exe
     if (scriptPath.endsWith('build/cli.exe')) {
       return SidekickPackage.fromDirectory(script.parent.parent)!;
     }
@@ -99,41 +103,41 @@ class SidekickContext {
     }
   }
 
-  /// The location of the entrypoint
+  /// The location of the entryPoint
   ///
-  /// Usually injected from the entrypoint itself via `env.SIDEKICK_ENTRYPOINT_HOME`
+  /// Usually injected from the entryPoint itself via `env.SIDEKICK_ENTRYPOINT_HOME`
   static File get entryPoint {
-    if (env.exists(_envEntrypointHome)) {
-      // CLI is called via entrypoint
-      final injectedEntryPointPath = env[_envEntrypointHome];
+    if (env.exists(_envEntryPointHome)) {
+      // CLI is called via entryPoint
+      final injectedEntryPointPath = env[_envEntryPointHome];
       if (injectedEntryPointPath == null || injectedEntryPointPath.isBlank) {
-        throw 'Injected entrypoint was not set (env.$_envEntrypointHome)';
+        throw 'Injected entryPoint was not set (env.$_envEntryPointHome)';
       }
-      final entrypoint =
+      final entryPoint =
           File(normalize('$injectedEntryPointPath/${core.cliName}'));
-      if (!entrypoint.existsSync()) {
-        throw 'Injected entrypoint does not exist ${entrypoint.absolute.path}';
+      if (!entryPoint.existsSync()) {
+        throw 'Injected entryPoint does not exist ${entryPoint.absolute.path}';
       }
-      return entrypoint;
+      return entryPoint;
     } else {
       // Fallback strategy: Search all parents directories for the entrypoint
       // This case is used when debugging the cli and the dart program is
       // started on the DartVM, and not called and compiled with the entrypoint
-      final entrypointName = core.cliNameOrNull ?? sidekickPackage.cliName;
+      final entryPointName = core.cliNameOrNull ?? sidekickPackage.cliName;
 
       Directory current = sidekickPackageDir;
       while (true) {
-        final entrypoint = current
+        final entryPoint = current
             .listSync()
             .whereType<File>()
-            .where((it) => it.name == entrypointName);
-        if (entrypoint.isNotEmpty) {
-          return entrypoint.single;
+            .where((it) => it.name == entryPointName);
+        if (entryPoint.isNotEmpty) {
+          return entryPoint.single;
         }
 
         final parent = current.parent;
         if (parent.path == current.path) {
-          throw "Can't find entrypoint $entrypointName from ${sidekickPackageDir.path}";
+          throw "Can't find entryPoint $entryPointName from ${sidekickPackageDir.path}";
         }
         current = parent;
       }
