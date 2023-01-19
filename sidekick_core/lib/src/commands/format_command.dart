0f4e6e9c70b9c11df25049f055d49d85128b3ca1
jxstxn__ <justin.baumann@t-online.de>
2023-01-19 20:22:13 +0100

    Add Format Command

---
+++ sidekick_core/lib/src/commands/format_command.dart
@@ -0,0 +1,136 @@
+import 'package:glob/glob.dart';
+import 'package:glob/list_local_fs.dart';
+import 'package:sidekick_core/sidekick_core.dart';
+import 'package:yaml/yaml.dart';
+
+/// Formats the Code for all Flutter/Dart packages in the repository
+class FormatCommand extends Command {
+  @override
+  String get description => 'Formatting the code for all packages';
+
+  @override
+  String get name => 'format';
+
+  /// packages whose files should not be formatted
+  final List<DartPackage> exclude;
+
+  /// glob patterns of packages whose dependencies should not be loaded
+  ///
+  /// Search starts at repository root.
+  ///
+  /// Example project layout:
+  ///
+  /// ```
+  /// repo-root
+  /// ├── packages
+  /// │   ├── package1
+  /// │   ├── package2
+  /// │   └── circle
+  /// └── third_party
+  ///     ├── circle
+  ///     │   ├── packageA
+  ///     │   └── packageB
+  ///     └── square
+  /// ```
+  ///
+  /// - Use `packages/package1/**` to exclude only `packages/package1`.
+  /// - Use `**/circle/**` to exclude `packages/circle` as well as
+  ///   `third_party/circle/packageA` and `third_party/circle/packageB`.
+  final List<String> excludeGlob;
+
+  FormatCommand({
+    this.exclude = const [],
+    this.excludeGlob = const [],
+  }) {
+    argParser.addOption(
+      'package',
+      abbr: 'p',
+    );
+    argParser.addOption(
+      'line-length',
+      abbr: 'l',
+    );
+  }
+
+  @override
+  Future<void> run() async {
+    final String? packageName = argResults?['package'] as String?;
+    final int? lineLength =
+        int.tryParse(argResults?['line-length'] as String? ?? '');
+
+    final List<DartPackage> allPackages = repository.findAllPackages();
+
+    if (packageName != null) {
+      final package =
+          allPackages.where((it) => it.name == packageName).firstOrNull;
+      if (package == null) {
+        throw "Package with name $packageName not found in repository "
+            "${repository.root.path}";
+      }
+      // only format for selected package
+      _format(package, globalLineLength: lineLength);
+      return;
+    }
+
+    final errorBuffer = StringBuffer();
+
+    final globExcludes = excludeGlob
+        .expand((rule) {
+          // start search at repo root
+          final root = repository.root.path;
+          return Glob("$root/$rule").listSync(root: root);
+        })
+        .whereType<Directory>()
+        .mapNotNull((e) => DartPackage.fromDirectory(e));
+
+    final excluded = [
+      ...exclude,
+      ...globExcludes,
+    ];
+
+    for (final package in allPackages.whereNot(excluded.contains)) {
+      try {
+        _format(package, globalLineLength: lineLength);
+      } catch (e, stack) {
+        print('Error while formatting Code for ${package.name} '
+            '(${package.root.path})');
+        errorBuffer.writeln("${package.name}: $e\n$stack");
+      }
+    }
+    final errorText = errorBuffer.toString();
+    if (errorText.isNotEmpty) {
+      printerr("\n\nErrors while formatting:");
+      printerr(errorText);
+      exitCode = 1;
+    }
+  }
+}
+
+void _format(DartPackage package, {int? globalLineLength}) {
+  print(yellow('=== package ${package.name} ==='));
+  final int exitCode;
+  final int lineLength = globalLineLength ?? getLineLength(package);
+  if (package.isFlutterPackage) {
+    exitCode = flutter(
+      ['format', '-l', '$lineLength', '.'],
+      workingDirectory: package.root,
+    );
+  } else {
+    exitCode = dart(
+      ['format', '-l', '$lineLength', '.'],
+      workingDirectory: package.root,
+    );
+  }
+  if (exitCode != 0) {
+    throw "Failed to get dependencies for package ${package.root.path}";
+  }
+  print("\n");
+}
+
+int getLineLength(DartPackage package) {
+  final yamlFile = package.root.file('pubspec.yaml').readAsStringSync();
+  final pubspecData = loadYaml(yamlFile) as YamlMap;
+  final mapData =
+      pubspecData.map((key, value) => MapEntry(key.toString(), value));
+  return (mapData['format'] as Map?)?['line_length'] as int? ?? 80;
+}
---
+++ sidekick_core/test/format_command_test.dart
@@ -0,0 +1,236 @@
+import 'package:sidekick_core/sidekick_core.dart';
+import 'package:sidekick_core/src/commands/format_command.dart';
+import 'package:sidekick_test/fake_stdio.dart';
+import 'package:sidekick_test/sidekick_test.dart';
+import 'package:test/test.dart';
+
+const String mainFileUnformatted = '''
+void main() {
+  final test = ['Hello', 'World', 'This', 'is', 'a', 'test', 'for', 'the', 'format', 'command'];
+}
+''';
+const String mainFileFormatted = '''
+void main() {
+  final test = [
+    'Hello',
+    'World',
+    'This',
+    'is',
+    'a',
+    'test',
+    'for',
+    'the',
+    'format',
+    'command'
+  ];
+}
+''';
+
+void main() {
+  void setupProject(
+    Directory tempDir, {
+    required String pubspecContent,
+    required String mainContent,
+  }) {
+    final pubspec = tempDir.file('pubspec.yaml')..createSync();
+    pubspec.writeAsStringSync(pubspecContent);
+    final mainFile = tempDir.file('lib/main.dart')..createSync(recursive: true);
+    mainFile.writeAsStringSync(mainContent);
+  }
+
+  group('getLineLength', () {
+    late File pubspecYamlFile;
+    late DartPackage package;
+
+    setUp(() {
+      final temp = Directory.systemTemp.createTempSync();
+      pubspecYamlFile = temp.file('pubspec.yaml')..writeAsStringSync('''
+name: dashi
+''');
+      env['SIDEKICK_PACKAGE_HOME'] = temp.path;
+      package = DartPackage.fromDirectory(temp)!;
+
+      addTearDown(() {
+        env['SIDEKICK_PACKAGE_HOME'] = null;
+        temp.deleteSync(recursive: true);
+      });
+    });
+    test(
+        'should return 80 as default if format argument is not present in pubspec',
+        () {
+      expect(getLineLength(package), 80);
+    });
+    test(
+        'should return 80 as default if line_length argument is not present in pubspec',
+        () {
+      pubspecYamlFile.writeAsStringSync('''
+name: dashi
+format:
+''');
+      expect(getLineLength(package), 80);
+    });
+    test('should return the set line_length from the pubspec if present', () {
+      pubspecYamlFile.writeAsStringSync('''
+name: dashi
+format:
+  line_length: 123
+''');
+      expect(getLineLength(package), 123);
+    });
+  });
+  group('Format Command', () {
+    test('Format the File to 80 if nothing else is set in Pubspec', () async {
+      await insideFakeProjectWithSidekick((dir) async {
+        final fakeStdOut = FakeStdoutStream();
+        await overrideIoStreams(
+          stdout: () => fakeStdOut,
+          body: () async {
+            setupProject(
+              dir,
+              pubspecContent: '''
+name: dashi
+''',
+              mainContent: mainFileUnformatted,
+            );
+            final runner = initializeSidekick(
+              name: 'dash',
+              dartSdkPath: systemDartSdkPath(),
+            );
+            runner.addCommand(FormatCommand());
+            await runner.run(['format']);
+
+            expect(exitCode, 0);
+            expect(
+              dir.file('lib/main.dart').readAsStringSync(),
+              mainFileFormatted,
+            );
+          },
+        );
+      });
+    });
+    test('Format the File to 120 if set as Command Argument', () async {
+      await insideFakeProjectWithSidekick((dir) async {
+        final fakeStdOut = FakeStdoutStream();
+        await overrideIoStreams(
+          stdout: () => fakeStdOut,
+          body: () async {
+            setupProject(
+              dir,
+              pubspecContent: '''
+name: dashi
+''',
+              mainContent: mainFileUnformatted,
+            );
+            final runner = initializeSidekick(
+              name: 'dash',
+              dartSdkPath: systemDartSdkPath(),
+            );
+            runner.addCommand(FormatCommand());
+            await runner.run(['format', '--line-length', '120']);
+
+            expect(exitCode, 0);
+            expect(
+              dir.file('lib/main.dart').readAsStringSync(),
+              mainFileUnformatted,
+            );
+          },
+        );
+      });
+    });
+    test('Format the File to 120 if set in PubspecYaml', () async {
+      await insideFakeProjectWithSidekick((dir) async {
+        final fakeStdOut = FakeStdoutStream();
+        await overrideIoStreams(
+          stdout: () => fakeStdOut,
+          body: () async {
+            setupProject(
+              dir,
+              pubspecContent: '''
+name: dashi
+format:
+  line_length: 120
+''',
+              mainContent: mainFileUnformatted,
+            );
+            final runner = initializeSidekick(
+              name: 'dash',
+              dartSdkPath: systemDartSdkPath(),
+            );
+            runner.addCommand(FormatCommand());
+            await runner.run(['format']);
+
+            expect(exitCode, 0);
+            expect(
+              dir.file('lib/main.dart').readAsStringSync(),
+              mainFileUnformatted,
+            );
+          },
+        );
+      });
+    });
+    test('Format the File to 120 if set in PubspecYaml', () async {
+      await insideFakeProjectWithSidekick((dir) async {
+        final fakeStdOut = FakeStdoutStream();
+        await overrideIoStreams(
+          stdout: () => fakeStdOut,
+          body: () async {
+            setupProject(
+              dir,
+              pubspecContent: '''
+name: dashi
+format:
+  line_length: 80
+''',
+              mainContent: mainFileUnformatted,
+            );
+            final runner = initializeSidekick(
+              name: 'dash',
+              dartSdkPath: systemDartSdkPath(),
+            );
+            runner.addCommand(FormatCommand());
+            await runner.run(['format']);
+
+            expect(exitCode, 0);
+            expect(
+              dir.file('lib/main.dart').readAsStringSync(),
+              mainFileFormatted,
+            );
+          },
+        );
+      });
+    });
+    test('Excluding all Files in lib/', () async {
+      await insideFakeProjectWithSidekick((dir) async {
+        final fakeStdOut = FakeStdoutStream();
+        await overrideIoStreams(
+          stdout: () => fakeStdOut,
+          body: () async {
+            setupProject(
+              dir,
+              pubspecContent: '''
+name: dashi
+''',
+              mainContent: mainFileUnformatted,
+            );
+            final runner = initializeSidekick(
+              name: 'dash',
+              dartSdkPath: systemDartSdkPath(),
+            );
+            runner.addCommand(
+              FormatCommand(
+                exclude: [DartPackage.flutter(dir.directory('lib'), 'Dash')],
+              ),
+            );
+            await runner.run(['format']);
+
+            expect(exitCode, 0);
+            expect(
+              dir.file('lib/main.dart').readAsStringSync(),
+              mainFileUnformatted,
+            );
+          },
+        );
+      });
+    });
+  });
+}
