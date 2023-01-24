154fa5d9d42d3df46cebd900d4b3a9fed105cdbf
jxstxn__ <justin.baumann@t-online.de>
2023-01-24 09:59:45 +0100

    Only show FormatCommand

--- sidekick_core/lib/sidekick_core.dart
+++ sidekick_core/lib/sidekick_core.dart
@@ -25,7 +25,8 @@ export 'package:sidekick_core/src/commands/bash_command.dart';
 export 'package:sidekick_core/src/commands/dart_command.dart';
 export 'package:sidekick_core/src/commands/deps_command.dart';
 export 'package:sidekick_core/src/commands/flutter_command.dart';
-export 'package:sidekick_core/src/commands/format_command.dart';
+export 'package:sidekick_core/src/commands/format_command.dart'
+    show FormatCommand;
 export 'package:sidekick_core/src/commands/install_global_command.dart';
 export 'package:sidekick_core/src/commands/plugins/plugins_command.dart';
 export 'package:sidekick_core/src/commands/recompile_command.dart';
