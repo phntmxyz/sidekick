37bf166a4d006e161aa3425ac6fbda1b0e126670
Justin Baumann <justin.baumann@t-online.de>
2023-02-14 13:46:46 +0100

    Adding migration for format command

---
+++ sidekick_core/lib/src/update/patches/192_add_format_command.patch.dart
@@ -0,0 +1,16 @@
+import 'package:sidekick_core/sidekick_core.dart';
+import 'package:sidekick_core/src/update/migration.dart';
+
+final addFormatCommand192 = MigrationStep.inline(
+  (context) async {
+    final mainFile = SidekickContext.sidekickPackage.cliMainFile;
+    final mainFileContent = mainFile.readAsLinesSync();
+    mainFileContent.insert(
+      mainFileContent.indexWhere((line) => line.contains('runner')) + 1,
+      "    ..addCommand(FormatCommand())",
+    );
+  },
+  name: 'Add a format command',
+  targetVersion: Version(1, 0, 0),
+  pullRequestLink: 'github.com/phntmxyz/sidekick/pull/192',
+);
