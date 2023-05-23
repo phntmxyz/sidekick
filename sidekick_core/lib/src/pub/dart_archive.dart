6cd0eca3c49c9736982bfdf49fd16325e13afab3
Pascal Welsch <pascal@phntm.xyz>
2023-05-23 02:39:11 +0200

    Fix analysis

--- sidekick_core/lib/src/pub/dart_archive.dart
+++ sidekick_core/lib/src/pub/dart_archive.dart
@@ -9,12 +9,13 @@ class DartArchive {
 
   Stream<Version> getLatestDartVersions() async* {
     final url = Uri.parse(
-        'https://storage.googleapis.com/storage/v1/b/dart-archive/o?delimiter=/&prefix=channels/stable/release/');
+      'https://storage.googleapis.com/storage/v1/b/dart-archive/o?delimiter=/&prefix=channels/stable/release/',
+    );
     final response = await get(url);
     if (response.statusCode != 200) {
       throw Exception('Received status code ${response.statusCode}');
     }
-    final json = jsonDecode(response.body);
+    final json = jsonDecode(response.body) as Map<dynamic, dynamic>;
 
     final prefixes = json['prefixes'] as List<dynamic>;
     for (final path in prefixes) {
