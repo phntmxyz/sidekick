77d90a99796762d041f69124e36394b825093d50
Pascal Welsch <pascal@phntm.xyz>
2023-09-14 01:38:52 +0200

    Increase timeouts

--- sidekick/test/tab_completion_test.dart
+++ sidekick/test/tab_completion_test.dart
@@ -28,36 +28,47 @@ void main() {
   setUp(uninstallGlobalCli);
   tearDown(uninstallGlobalCli);
 
-  test('Prints info when tab completions are not installed', () async {
-    await withSidekickCli((cli) async {
-      final p = await cli.run([]);
-
-      final command = yellow('dashi sidekick install-global');
-      final expectedMessage =
-          '${cyan('Run')} $command ${cyan('to enable tab completion.')}';
-
-      final stdout = await p.stdoutStream().toList();
-      expect(stdout, contains(expectedMessage));
-    });
-  });
-
-  test('Tab prints completions', () async {
-    await withSidekickCli(
-      (cli) async {
-        await expectLater(
-          'dashi',
-          cli.suggests({
-            'dart': 'Calls dart',
-            'deps': 'Gets dependencies for all packages',
-            'clean': 'Cleans the project',
-            'analyze': 'Dart analyzes the whole project',
-            'format': 'Formats all Dart files in the repository.',
-            'sidekick': 'Manages the sidekick CLI',
-            '--help': 'Print this usage information.',
-            '--version': 'Print the sidekick version of this CLI.',
-          }),
+  test(
+    'Prints info when tab completions are not installed',
+    () async {
+      await withSidekickCli((cli) async {
+        final p = await cli.run([]);
+        final stdout = await p.stderrStream().toList();
+        expect(
+          stdout,
+          containsAllInOrder([
+            '💡Tip:',
+            'Run',
+            './dashi sidekick install-global',
+            'to enable tab completion',
+          ]),
         );
-      },
-    );
-  });
+      });
+    },
+    timeout: const Timeout(Duration(seconds: 90)),
+  );
+
+  test(
+    'Tab prints completions',
+    () async {
+      await withSidekickCli(
+        (cli) async {
+          await expectLater(
+            'dashi',
+            cli.suggests({
+              'dart': 'Calls dart',
+              'deps': 'Gets dependencies for all packages',
+              'clean': 'Cleans the project',
+              'analyze': 'Dart analyzes the whole project',
+              'format': 'Formats all Dart files in the repository.',
+              'sidekick': 'Manages the sidekick CLI',
+              '--help': 'Print this usage information.',
+              '--version': 'Print the sidekick version of this CLI.',
+            }),
+          );
+        },
+      );
+    },
+    timeout: const Timeout(Duration(seconds: 90)),
+  );
 }
