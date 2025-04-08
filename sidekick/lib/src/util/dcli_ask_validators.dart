975fcd4ee14e6e57b94efe8d1923fc424de3e56a
Maikel Rehl <maikel@phntm.xyz>
2025-04-08 16:36:58 +0200

    Update to dcli 7.0.2

--- sidekick/lib/src/util/dcli_ask_validators.dart
+++ sidekick/lib/src/util/dcli_ask_validators.dart
@@ -6,9 +6,11 @@ class CliNameValidator extends dcli.AskValidator {
   const CliNameValidator();
 
   @override
-  String validate(String line) {
+  String validate(String line, {String? customErrorMessage}) {
     if (!isValidPubPackageName(line)) {
-      throw dcli.AskValidatorException(dcli.red(invalidCliNameErrorMessage));
+      throw dcli.AskValidatorException(
+        dcli.red(customErrorMessage ?? invalidCliNameErrorMessage),
+      );
     }
 
     return line;
@@ -26,14 +28,16 @@ class DirectoryExistsValidator extends dcli.AskValidator {
   const DirectoryExistsValidator();
 
   @override
-  String validate(String line) {
+  String validate(String line, {String? customErrorMessage}) {
     // TODO: this is an unnecessarily verbose version of just Directory(line).
     // However, with Directory(line) I can't get the test 'returns path when directory exists (relative path)'
     // to work because I must be missing some value in IOOverrides
     final dir =
         Directory(Context(current: Directory.current.path).canonicalize(line));
     if (!dir.existsSync()) {
-      throw AskValidatorException('The directory $line does not exist.');
+      throw AskValidatorException(
+        customErrorMessage ?? 'The directory $line does not exist.',
+      );
     }
     return line;
   }
@@ -46,11 +50,12 @@ class DirectoryIsWithinOrEqualValidator extends dcli.AskValidator {
   final Directory root;
 
   @override
-  String validate(String line) {
+  String validate(String line, {String? customErrorMessage}) {
     final dir = Directory(Context(current: root.path).canonicalize(line));
     if (!dir.isWithinOrEqual(root)) {
       throw AskValidatorException(
-        'The directory ${dir.path} must be within or equal to ${root.canonicalized.path}.',
+        customErrorMessage ??
+            'The directory ${dir.path} must be within or equal to ${root.canonicalized.path}.',
       );
     }
     return line;
